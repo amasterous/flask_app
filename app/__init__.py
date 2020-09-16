b46807fadf06ee0a0fcf732ef271db2d45fcb05d
Daniil <danuas@yandex.ru>
2020-09-16 11:52:46 +0300

    11

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Log in to see this page."
 
 from app import routes, models
